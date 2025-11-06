941a28662756154d7c35e835f1e3d92ed235ce2c
nponchon <nponchon@student.42.fr>
2025-11-06 17:59:17 +0100

    Sally on the way, need to write the main, then handle the string using the decrementation

---
+++ C/Sally/Sally.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define X 5
+#define STR "#include <stdio.h>%1$c#define FILENAME %3$cGrace_kid.c%3$c%1$c#define STR %3$c%4$s%3$c%1$c#define MAIN() int main() { FILE *fp = fopen(FILENAME, %3$cw%3$c); fprintf(fp, STR, 10, 9, 34, STR); fclose(fp); return 0; }%1$c/*%1$c%2$cThis program will print its own source when run.%1$c*/%1$cMAIN()"
+
+int main() {
+	FILE *fp = fopen("Sally_kid.c", "w");
+	fprintf(fp, STR, 10, 9, 34, STR);
+	fclose(fp);
+	return 0;
+}
