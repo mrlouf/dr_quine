01e5a767a21acbe7775830d5bd5458582937d480
Nicolas (laptop) <ponchon.nicolas@gmail.com>
2026-04-09 16:29:21 +0200

    fix: fix Makefile for ASM, placeholder for Grace and Sully

---
+++ ASM/Sully/Sully.s
@@ -0,0 +1,17 @@
+global main
+extern printf
+
+section .rodata
+msg db "Sully starter (Linux NASM)\n", 0
+
+section .text
+main:
+	enter 0, 0
+	lea rdi, [rel msg]
+	xor eax, eax
+	call printf
+	xor eax, eax
+	leave
+	ret
+
+section .note.GNU-stack noalloc noexec nowrite progbits
